bd4c968df41384ce4265ae2bd9546c8e7b4292b5
EvilVase <Sharov.sasha@gmail.com>
2023-03-01 16:47:42 +0300

    dublicate fix
    убрал повторение namespace'а

--- stage_cell_object.cpp
+++ stage_cell_object.cpp
@@ -19,24 +19,4 @@ namespace Example
 		return this->_is_blocker;
 	}
 
-#include "constants.hpp"
-#include "stage_cell_object.hpp"
-
-
-namespace Example
-{
-	StageCellObjectType StageCellObject::GetType() const
-	{
-		return this->_type;
-	}
-
-	bool StageCellObject::IsPickable() const
-	{
-		return this->_is_pickupable;
-	}
-
-	bool StageCellObject::IsBlocker() const
-	{
-		return this->_is_blocker;
-	}
 }
