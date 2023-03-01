c603a289b694f791a28f132d6019b79dca1b455b
Евгений Заонегин <zaonegin_s@mail.ru>
2023-03-01 15:08:19 +0300

    Убрано дублирование строк

--- sprite.hpp
+++ sprite.hpp
@@ -1,28 +1,3 @@
-
-#ifndef __SPRITE_HPP__
-#define __SPRITE_HPP__
-
-
-#include "raylib.h"
-#include "library_of_textures.hpp"
-
-
-namespace Example
-{
-    class Sprite
-    {
-    public:
-        Sprite(const int& texture_id);
-        
-        void DrawAt(const Vector2& position, const TextureLibrary& texture_library) const;
-        
-    private:
-        Vector2 _center_position;
-        int _texture_id;
-    };
-}
-
-
 #ifndef __SPRITE_HPP__
 #define __SPRITE_HPP__
 
